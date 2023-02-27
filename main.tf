4fb730f9cf08f9bfd6ebfa7f3beff9555c66793b
Philip Anyaehie <philiplaw0077@yahoo.com>
2023-02-27 00:12:47 +0000

    modified main file

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
 
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.55.0"
-    }
-  }
-}
-
+# Provider Block
 provider "aws" {
   # Configuration options
 }
 
+
+# VPC Block
 resource "aws_vpc" "nginx-vpc" {
   cidr_block           = "10.28.0.0/16"
   enable_dns_support   = "true"
@@ -24,12 +17,16 @@ resource "aws_vpc" "nginx-vpc" {
   }
 }
 
+
+# Local Variables referenced for deploying resources below
 locals {
   subnets = { "alb-subnet-a" = { availability_zone = "eu-west-2a", cidr_block = "10.28.0.0/24", server = "nginx-server-a" },
   "alb-subnet-b" = { availability_zone = "eu-west-2b", cidr_block = "10.28.5.0/24", server = "nginx-server-b" } }
 
 }
 
+
+# Subnets block - Create two subnets in the nginx VPC
 resource "aws_subnet" "alb-subnets" {
   vpc_id                  = aws_vpc.nginx-vpc.id         // Referencing to the id of the VPC from the vpc resource block
   cidr_block              = each.value.cidr_block        // Referencing to the local variable
@@ -42,22 +39,26 @@ resource "aws_subnet" "alb-subnets" {
   }
 }
 
+
 resource "aws_internet_gateway" "alb-igw" {
-  vpc_id = aws_vpc.nginx-vpc.id
+  vpc_id = aws_vpc.nginx-vpc.id // Subnets uses this IGW to reach internet
 }
+
 
 resource "aws_route_table" "alb-rt-main" {
   vpc_id = aws_vpc.nginx-vpc.id
   route {
-    cidr_block = "0.0.0.0/0"                     // Associated subnets can reach everywhere
-    gateway_id = aws_internet_gateway.alb-igw.id // ALB-RT uses this IGW to reach internet
+    cidr_block = "0.0.0.0/0" // Associated subnets can reach everywhere
+    gateway_id = aws_internet_gateway.alb-igw.id
   }
+
   tags = {
     Name = "alb-public-rt"
   }
 }
 
-resource "aws_route_table_association" "alb-rt-subneta-association" {
+
+resource "aws_route_table_association" "alb-rt-subnets-association" {
   subnet_id      = aws_subnet.alb-subnets[each.key].id
   route_table_id = aws_route_table.alb-rt-main.id
   for_each       = local.subnets
@@ -65,14 +66,10 @@ resource "aws_route_table_association" "alb-rt-subneta-association" {
 }
 
 
+# Security group allows HTTP, SSH
 resource "aws_security_group" "ec2-alb-acess" {
   vpc_id = aws_vpc.nginx-vpc.id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 
   ingress {
     from_port   = 80
@@ -88,28 +85,48 @@ resource "aws_security_group" "ec2-alb-acess" {
     cidr_blocks = ["0.0.0.0/0"] // To allow EC2 instance connect to test Nginx status.
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "nginx-server-alb-sg"
   }
 }
 
 
+# Deploys two instances with nginx bootstrapped
 resource "aws_instance" "nginx-servers" {
   subnet_id     = aws_subnet.alb-subnets[each.key].id
   ami           = "ami-09ee0944866c73f62"
   instance_type = "t2.micro"
+  key_name      = "London02"
   for_each      = local.subnets
+
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo amazon-linux-extras install nginx1 -y
+                sudo systemctl enable nginx
+                sudo systemctl start nginx
+                EOF
 
   tags = {
     Name = each.value.server
   }
 
   # Security Group
-  vpc_security_group_ids = ["${aws_security_group.ec2-alb-acess.id}"]
+  vpc_security_group_ids = [aws_security_group.ec2-alb-acess.id]
 
 }
 
-output "nginx-servers-id" {
+
+# Outputs the public ips of both servers
+output "nginx-servers" {
   description = "ID of nginx servers"
   value       = aws_instance.nginx-servers
 }
